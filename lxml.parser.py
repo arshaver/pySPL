6ca753241bfedda5f5a8d3eeeb90289fe1081d17
Anthony Shaver <anthony.r.shaver@gmail.com>
2011-04-01 09:29:49 -0700

    comment updates, and made path to labels a relative path

--- lxml.parser.py
+++ lxml.parser.py
@@ -2,9 +2,8 @@ from lxml import etree
 from datetime import date
 import csv
 
+#gets all XML files from a directory and returns them as a list with full directory
 def get_xml_files(dir):
-    """gets all XML files from a directory and
-    returns them as a list with full directory"""
     import os
     filename_list = []
     
@@ -13,15 +12,15 @@ def get_xml_files(dir):
             filename_list.append(dir+file)
     return filename_list
 
+#returns a python list of all active moieties listed in the file
 def get_actives(drug):
-    """returns a python list of all active moieties listed in the file"""
     actives = []
     for item in drug.findall("//{urn:hl7-org:v3}activeMoiety/{urn:hl7-org:v3}activeMoiety/{urn:hl7-org:v3}name"):
         actives.append(item.text)
     #here converting to a set removes duplicates
     return list(set(actives))
 
-#start marketing date    
+#start *marketing* date (much larger range than get_revision_date() function below)
 def get_start_date(drug):
     date_string = drug.findall("//{urn:hl7-org:v3}effectiveTime/{urn:hl7-org:v3}low")[0].attrib["value"]
     year = int(date_string[0:4])
@@ -29,7 +28,7 @@ def get_start_date(drug):
     day = int(date_string[6:8])
     return date(year,month,day).strftime("%b %d, %Y")
 
-#label revision date
+#label *revision* date
 def get_revision_date(drug):
     date_string = drug.findall("//{urn:hl7-org:v3}effectiveTime")[0].attrib["value"]
     year = int(date_string[0:4])
@@ -41,7 +40,7 @@ def get_type(drug):
     drug_type = drug.findall("//{urn:hl7-org:v3}code")[0].attrib["displayName"]
     return drug_type
 
-#takes file input, not etree
+#takes file input, not etree, because it checks the file line by line
 def check_warfarin(file):
     drug = open(file)
     for line in drug:
@@ -65,6 +64,7 @@ def get_name(drug):
 def get_url(file):
     #first split to get the file name from the whole path, then split to get rid of the .xml
     file_name = str(file.split("/")[-1]).split(".")[0]
+    #then build the url
     url = "http://www.accessdata.fda.gov/spl/data/"+file_name+"/"+file_name+".xml"
     return url
     
@@ -75,7 +75,7 @@ def get_url(file):
 
 #lists full path; NEED trailing slash here
 #this directory contains all of the results (eg the *.xml files) of this query: http://labels.fda.gov/getIngredientName.cfm?beginrow=1&numberperpage=2557&searchfield=acetaminophen&OrderBy=IngredientName
-file_list = get_xml_files("/Users/anthonyshaver/Documents/Pharmacy School/apap warfarin interaction project/all_apap_labels/")
+file_list = get_xml_files("./all_apap_labels/")
 
 with open('output.csv', 'wb') as f:
     #pipe delimiter is so we can use commas elsewhere; nobody uses pipes and excel/google docs don't care

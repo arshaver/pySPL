b34b1fb6fb1bacdba0cd6a467c9dc687e55627d4
Anthony Shaver <anthony.r.shaver@gmail.com>
2011-11-11 19:46:22 -0800

    refactored nearly all the code to use xpath

--- apap.py
+++ apap.py
@@ -1,43 +1,50 @@
-from pySPL import *
+from pySPL import DrugLabel
 from utils import file_list
+import csv
 
-#
+#don't bother adding marketing end date, it's NONE for all labels
+
 with open('output.csv', 'wb') as f:
     #pipe delimiter is so we can use commas elsewhere; nobody uses pipes and excel/google docs do not care
     writer = csv.writer(f,delimiter="|")
     
     #writes header row. need to keep this the same as how it's written below
     writer.writerow(
-        ["drug name",
+        ["distributor",
+        "drug name",
         "ndc",
+        "label type",
+        "dosage form",
         "marketing start date",
         "revision date",
-        "type",
         "warfarin mentioned?",
-        "warfarin line",
-        "dosage form",
         "#actives",
-        "active compounds",
+        "actives",
         "url",
         ])
-    for xml_file in file_list:
-        xml = etree.parse(xml_file)
+    count = 1
+    for xml_label in file_list:
+        print "label ",count," of ",len(file_list),"(%s)" % xml_label
+        label = DrugLabel(xml_label)
         #ignore kits
-        if get_dosage_form(xml) == "KIT":
+        if label.dosage_form() == "KIT" or label.dosage_form() == "BULK INGREDIENT":
+            print "passing on %s " % xml_label
             pass
         else:
             writer.writerow([
-                get_name(xml),
-                get_ndc(xml),
-                get_start_date(xml),
-                get_revision_date(xml),
-                get_label_type(xml),
-                check_word(xml_file,"warfarin"),
-                get_word_line(xml_file,"warfarin"),
-                get_dosage_form(xml),
-                len(get_actives(xml)),
-                get_actives(xml),
-                get_url(xml_file),
+                label.distributor(),
+                label.name(),
+                label.ndc(),
+                label.label_type(),
+                label.dosage_form(),
+                label.start_date(),
+                label.revision_date(),
+                label.check_word("warfarin"),
+                len(label.actives()),
+                label.actives(),
+                label.build_url(),
                 ])
+        count+=1
+    print "success!"
 
 f.close()
--- pySPL.py
+++ pySPL.py
@@ -1,90 +1,81 @@
 from lxml import etree
 from datetime import date
 
-namespaces={"a":"urn:hl7-org:v3",}
+#http://www.accessdata.fda.gov/spl/stylesheet/spl-common.xsl
+namespaces={"v3":"urn:hl7-org:v3",}
+
+def normalize_date(date_string):
+    year = int(date_string[0:4])
+    month = int(date_string[4:6])
+    day = int(date_string[6:8])
+    return date(year,month,day).strftime("%b %d, %Y")
 
 class DrugLabel(object):
-    """represents a DrugLabel in the SPL format"""
+    """represents a Drug Label in the SPL format.
+    takes one argument, spl_label, which can be either an url or a file"""
     
     def __init__(self, spl_label):
-        self.url = spl_label
+        self.label_data = spl_label
         self.xml = etree.parse(spl_label)
-    
+            
     def actives(self):
-        return list(set(active.text for active in self.xml.xpath("//a:activeMoiety//a:activeMoiety/a:name",namespaces=namespaces)))
+        """returns a list of active compounds"""
         #here converting to a set removes duplicates
+        return list(set(active.text for active in self.xml.xpath("//v3:ingredientSubstance/v3:activeMoiety/v3:activeMoiety/v3:name",namespaces=namespaces)))
 
     def start_date(self):
         """returns start marketing date as a strftime formatted python date object"""
-        date_string = self.xml.findall("//{urn:hl7-org:v3}effectiveTime/{urn:hl7-org:v3}low")[0].attrib["value"]
-        year = int(date_string[0:4])
-        month = int(date_string[4:6])
-        day = int(date_string[6:8])
-        return date(year,month,day).strftime("%b %d, %Y")
+        date_string = self.xml.xpath("//v3:subjectOf/v3:marketingAct/v3:effectiveTime/v3:low/@value",namespaces=namespaces)[0]
+        return normalize_date(date_string)
+
+    def end_date(self):
+        """returns end marketing date as a strftime formatted python date object or None if not defined
+        refers to the expiration date of the last lot released to the market
+        (from http://spl-work-group.wikispaces.com/file/view/creating_otc_sp_documentsl.pdf)"""
+        try:
+            date_string = self.xml.xpath("//v3:subjectOf/v3:marketingAct/v3:effectiveTime/v3:high/@value",namespaces=namespaces)[0]
+            return normalize_date(datestring)
+        except:
+            return "None"
+
+    def marketing_category(self):
+        """returns the marketing category"""
+        return self.xml.xpath("//v3:subjectOf/v3:approval/v3:code/@displayName",namespaces=namespaces)[0]
 
     def revision_date(self):
         """returns label revision date"""
-        date_string = self.xml.findall("//{urn:hl7-org:v3}effectiveTime")[0].attrib["value"]
-        year = int(date_string[0:4])
-        month = int(date_string[4:6])
-        day = int(date_string[6:8])
-        return date(year,month,day).strftime("%b %d, %Y")
+        date_string = self.xml.xpath("/v3:document/v3:effectiveTime/@value",namespaces=namespaces)[0]
+        return normalize_date(date_string)
 
     def label_type(self):
         """returns the drug label type, typically 'HUMAN OTC DRUG LABEL' or 'HUMAN PRESCRIPTION DRUG LABEL' """
-        drug_type = self.xml.findall("//{urn:hl7-org:v3}code")[0].attrib["displayName"]
-        return drug_type
+        return self.xml.xpath("//v3:code/@displayName",namespaces=namespaces)[0]
 
     def ndc(self):
-        "returns the drug's NDC number"
-        ndc = self.xml.find("//{urn:hl7-org:v3}manufacturedProduct/{urn:hl7-org:v3}code").attrib["code"]
-        return ndc
+        """returns the drug's NDC number"""
+        #xpath query is NOT from the xsl file
+        return self.xml.xpath("//v3:manufacturedProduct/v3:manufacturedProduct/v3:code/@code",namespaces=namespaces)[0]
 
-    #TODO check to make sure full name is being returned (eg maybe other tags interfering)
     def name(self):
         """returns the drug's name"""
-        drug_name = self.xml.find("//{urn:hl7-org:v3}manufacturedProduct/{urn:hl7-org:v3}name").text
-        return drug_name
-
+        return self.xml.xpath("//v3:manufacturedProduct/v3:manufacturedProduct/v3:name",namespaces=namespaces)[0].text
+    
+    def distributor(self):
+        """returns the drug's distributor"""
+        return self.xml.xpath("//v3:author/v3:assignedEntity/v3:representedOrganization/v3:name",namespaces=namespaces)[0].text
+    
     def dosage_form(self):
         """returns the drug's dosage form"""
-        dosage_form = self.xml.find("//{urn:hl7-org:v3}manufacturedProduct/{urn:hl7-org:v3}manufacturedProduct/{urn:hl7-org:v3}formCode").attrib["displayName"]
-        return dosage_form
+        return self.xml.xpath("//v3:manufacturedProduct/v3:manufacturedProduct/v3:formCode/@displayName",namespaces=namespaces)[0]
 
-
-    #the next two functions take file input, not lxml.etree inputs
-
-    def check_word(file, word):
-        """
-        returns True if the string "word" or "Word" or "WORD" is found in any line in the file
-        useful for checking if another drug, such as an interaction, is mentioned
-        """
-        drug = open(file)
-        word = str(word).lower()
-        for line in drug:
-            if (word in line) or (word.capitalize() in line) or (word.upper() in line):
-                return True
-                break
-        return False
-
-    def get_word_line(file, word):
-        """
-        returns the whitespace-stripped line that contains "word" or "Word" or "WORD"
-        useful for checking if the "word" listing is legitimate for your purposes
-        not that this only displays the first line found containing "word" or "Word" or "WORD"
-        """
-        drug = open(file)
-        word = str(word).lower()
-        for line in drug:
-            if (word in line) or (word.capitalize() in line) or (word.upper() in line):
-                return line.strip()
-                break
-        return False
-
-    def build_url(file):
-        """builds and returns the accessdata.fda.gov URL given the XML file name/directory"""
-        #first split to get the file name from the whole path, then split to get rid of the .xml
-        file_name = str(file.split("/")[-1]).split(".")[0]
-        #then build the url
-        url = "http://www.accessdata.fda.gov/spl/data/%s/%s.xml" (file_name,file_name)
-        return url
+    def build_url(self):
+        """helper function that builds and returns the accessdata.fda.gov URL given the XML file name/directory"""
+        #maybe won't work on windows because slash direction?
+        guid = self.label_data.split("/")[-1].split(".")[0]
+        return "http://www.accessdata.fda.gov/spl/data/%s/%s.xml" %(guid,guid)
+    
+    def check_word(self, word):
+        """checks if the xml contains 'word','Word' or 'WORD' and returns True if found or False if not"""
+        word = str(word)
+        query = "//*[text()[contains(.,'%s') or contains(.,'%s') or contains(.,'%s')]]" %(word.lower(),word.upper(),word.capitalize())
+        return True if self.xml.xpath(query,namespaces=namespaces) else False
